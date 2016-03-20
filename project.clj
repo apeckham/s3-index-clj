c4bac17a947d95d0d1f2a1aa0e487b00aa548f00
apeckham <apeckham@gmail.com>
2016-03-19 18:23:59 -0700

    add amazonica

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [amazonica "0.3.52"]]
   :main ^:skip-aot s3-index.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
