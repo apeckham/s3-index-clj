978488c46476baf08176605b4b8504af9b940ddc
apeckham <apeckham@gmail.com>
2016-03-19 18:38:42 -0700

    add lazy recursive list-s3 function

--- src/s3_index/core.clj
+++ src/s3_index/core.clj
@@ -1,7 +1,15 @@
 (ns s3-index.core
   (:gen-class))
 
+(defn list-s3
+  [request]
+  (let [response (amazonica.aws.s3/list-objects request)
+        object-summaries (:object-summaries response)
+        next-request (assoc request :marker (:next-marker response))]
+    (if (:truncated? response)
+      (concat object-summaries (lazy-seq (list-s3 next-request)))
+      object-summaries)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (prn 5))
