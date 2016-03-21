64d8308bb5e0cac4569e620ea2ad401fc38a0daa
apeckham <apeckham@gmail.com>
2016-03-20 18:49:02 -0700

    show previous/next buttons

--- src/s3_index/core.clj
+++ src/s3_index/core.clj
@@ -13,23 +13,29 @@
 
 (def object-url #(str "https://s3.amazonaws.com/" (:bucket-name %) "/" (:key %)))
 
+(defn li
+  [object]
+  [:li [:img.img-responsive {:src (object-url object)}]])
+
 (defn page
-  [objects]
-  (let [li (fn [object]
-             [:li [:img.img-responsive {:src (object-url object)}]])]
-    (hiccup.page/html5 [:head
-                        (hiccup.page/include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
-                        (hiccup.page/include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css")
-                        (hiccup.page/include-js "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js")
-                        [:style "body { background-color: lightgrey }"]]
-                       [:body
-                        [:ul.list-unstyled (map li objects)]])))
+  [index objects]
+  (hiccup.page/html5 [:head
+                      (hiccup.page/include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
+                      (hiccup.page/include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css")
+                      (hiccup.page/include-js "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js")
+                      [:style "body { background-color: lightgrey }"]]
+                     [:body
+                      [:nav
+                       [:ul.pager
+                        [:li [:a {:href (str "page" (dec index) ".html")} "Previous"]]
+                        [:li [:a {:href (str "page" (inc index) ".html")} "Next"]]]]
+                      [:ul.list-unstyled (map li objects)]]))
 
 (defn -main
   [& args]
-  (doseq [[index partition] (->> (list-all-objects {:bucket-name "***REMOVED***"})
-                                 #_(sort-by :last-modified)
-                                 (partition-all 50)
-                                 (take 1)
-                                 (map-indexed vector))]
-    (spit (str "/tmp/page" index ".html") (page partition))))
+  (doseq [[index objects] (->> (list-all-objects {:bucket-name "***REMOVED***"})
+                               #_(sort-by :last-modified)
+                               (partition-all 50)
+                               (take 1)
+                               (map-indexed vector))]
+    (spit (str "/tmp/page" index ".html") (page index objects))))
